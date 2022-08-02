e5598174f738c7b25bcc7b967b19609ffc188287
marcelojunior1 <marcelojunior@usp.br>
2022-08-02 19:27:17 -0300

    EP - Caso base

--- geocomp/projeto_5/embrulho_presente.py
+++ geocomp/projeto_5/embrulho_presente.py
@@ -3,43 +3,49 @@ from math import inf
 from geocomp.common.prim import *
 
 
-def Esquerda(self, i, j):
-    A = self.lista[i]
-    B = self.lista[j]
-
-    det = area2(A.init, A.to, B.init)
-
-    if A.init.x > B.init.x:
-        return not self.Esquerda(j, i)
-
-    if (A.init.x == B.init.x) and (j > i):
-        return not self.Esquerda(j, i)
-
-    if det == 0:
-        det = area2(A.init, A.to, B.to)
-
-        if det == 0:
-            return B.init.x > A.init.x
-
-    return det > 0
-
-
 def Embrulho_Presente(l):
     print("\n")
     n = len(l)
-    h = menor(l)
-    i = h + 1
-    print("H: ", h)
-    while i%n != h:
-        print(i)
-        i += 1
+    h = 0
+    H = [posicao_menor(l)]
+    cond = True
+
+    while cond:
+        i = (H[h] % n) + 1
+
+        if i == n:
+            i = 0
+
+        for j in range(0, n):
+            if i != j and j != H[h] and not Esquerda(H[h], i, j, l):
+                i = j
+
+        h += 1
+        H.append(i)
+        cond = (i != H[0])
+
+    print(H)
 
 
-def menor(l):
+def posicao_menor(l):
     p_i = 0
     for i in range(1, len(l)):
         ponto = l[i]
         if ponto.x < l[p_i].x:
             p_i = i
-
     return p_i
+
+
+def Esquerda(i, j, k, l):
+    A = l[i]
+    B = l[j]
+    C = l[k]
+
+    det = area2(A, B, C)
+
+    # Colineares
+    if det == 0:
+        print("Colineares: ", i, j, k)
+        return True
+
+    return det > 0
